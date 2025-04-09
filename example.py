d6b6e68467425d813c6000745f7e3f01dba50c61
vagrant <chocopie0619@gmail.com>
2025-04-10 01:18:52 +0800

    Make a .py file

--- example.py
+++ example.py
@@ -1,5 +1,13 @@
-from einsteinpy.plotting import GeodesicPlotter
-from einsteinpy.examples import perihelion
-a = GeodesicPlotter()
-a.plot(perihelion())
-a.show()
+import sympy
+from sympy import symbols, sin, cos, sinh
+from einsteinpy.symbolic import EinsteinTensor, MetricTensor
+
+sympy.init_printing()
+
+syms = sympy.symbols("t chi theta phi")
+t, ch, th, ph = syms
+m = sympy.diag(-1, cos(t) ** 2, cos(t) ** 2 * sinh(ch) ** 2, cos(t) ** 2 * sinh(ch) ** 2 * sin(th) ** 2).tolist()
+metric = MetricTensor(m, syms)
+
+einst = EinsteinTensor.from_metric(metric)
+einst.tensor()
